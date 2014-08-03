3f798eb68f4739b70295e1a39094a7662db75237
gabe <balcides@gmail.com>
2014-08-02 21:10:06 -0700

    added checkboxes

--- setRenderTool.py
+++ setRenderTool.py
@@ -1,9 +1,13 @@
 import maya.cmds as cmds
 import maya.mel as mel
-	
-uWidth = 250
+from functools import partial
 
-# BUTTONS ======================================
+uWidth = 250
+uAlign='right'
+
+print "loading script..."
+
+# BUTTON SCRIPTS ======================================
 
 def enablePrimaryVis(*args):
 	print 'Render Vis enabled'
@@ -21,21 +25,68 @@ def cleanupScene(*args):
 
 #COMMANDS ===============================================
 
+def getAttributes():
+	#attribute = 'primaryVisibility'
+	attributes = ['castsShadows','receiveShadows','motionBlur','primaryVisibility','visibleInReflections','visibleInRefractions','doubleSided']
+	selection = cmds.ls(sl=True)
+	for sel in selection:
+		print sel
+		relatives = cmds.listRelatives(sel, shapes=True)
+		for rel in relatives:
+			for attribute in attributes:
+				try:
+					this = cmds.getAttr(rel + '.' + attribute)
+					print attribute + " = " + str(this)
+					#cmds.checkBox(label='Primary Visibility', value=this)
+				except:
+					print "doesnt have " + attribute
+	print ""
+
 #Restores common attributes unneeded for rendering
 def setAttributes(*args):   
 	selection = cmds.ls(sl=True)
 	for obj in selection:
 	    #cmds.setAttr( obj+'.primaryVisibility', 1)
 	    cmds.setAttr( obj+'.'+ args[0], args[1])
-	    
+		
+#Updates the checkboxes on selection
+def updateAttr():
+	#cmds.scriptJob( killAll=True, force=True)
+	jobNum = cmds.scriptJob( event= ["SelectionChanged", getAttributes], protected=False)
+	jobs = cmds.scriptJob( listJobs=True )
+	#print jobs
+
+def setCheckBox(Label, CommandAttribute):
+	return cmds.checkBox( label=Label, align=uAlign, value=True, onCommand=partial (setAttributes, CommandAttribute, 1),  offCommand=partial (setAttributes, CommandAttribute, 0))
+
+	
+def printtest():
+	print 'this is working'
+
+cmds.scriptJob( killAll=True, force=True)
+updateAttr()
+	
 #LAUNCH WINDOW ========================================
 def launchTool():
-	myWindow = cmds.window( title='Compass Tools', width=uWidth )
-	cmds.columnLayout( adjustableColumn=True )
-	cmds.button( label='Enable Primary Visibility', command=enablePrimaryVis )
-	cmds.button( label='Disable Primary Visibility', command=disablePrimaryVis )
+	#if(cmds.window(myWindow, exists=True):
+		#cmds.deleteUI(myWindow)
+	myWindow = cmds.window( title='Compass Render Tools', width=uWidth )
+	myLayout = cmds.columnLayout( adjustableColumn=True, columnAlign=uAlign)
+	setCheckBox('Casts Shadow', 'castsShadows')
+	setCheckBox('Recieves Shadows', 'receiveShadows')
+	setCheckBox('Motion Blur', 'motionBlur')
+	setCheckBox('Primary Visibility', 'primaryVisibility')
+	setCheckBox('Visible In Reflections', 'visibleInReflections')
+	setCheckBox('Visible In Refractions', 'visibleInRefractions')
+	setCheckBox('Double Sided', 'doubleSided')
+	cmds.separator( height=10, style='in' )
 	cmds.button( label='Scene Cleanup', command=cleanupScene )
-	cmds.showWindow()
+	cmds.button( label='Render Scene', command='cmds.RenderIntoNewWindow()')
+	print myWindow
+	#print cmds.window(myWindow, frontWindow=True)
+	cmds.showWindow(myWindow)
 	allowedAreas = ['right', 'left']
 	#cmds.dockControl( area='left', content=myWindow, allowedArea=allowedAreas, label='testing', width=uWidth )
 	
+
+
